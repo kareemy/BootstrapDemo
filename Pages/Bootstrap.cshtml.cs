50a72570cb70de8c1bee6c2f4a7a37eacff66659
Kareem Dana <kdana@wtamu.edu>
2022-08-26 08:18:28 -0500

    Update to .NET 6 part 1

--- Pages/Bootstrap.cshtml.cs
+++ Pages/Bootstrap.cshtml.cs
@@ -1,30 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
-namespace BootstrapDemo.Pages
+namespace BootstrapDemo.Pages;
+
+public class BootstrapModel : PageModel
 {
-    public class BootstrapModel : PageModel
+    private readonly ILogger<BootstrapModel> _logger;
+
+    public BootstrapModel(ILogger<BootstrapModel> logger)
     {
-        private readonly ILogger<BootstrapModel> _logger;
+        _logger = logger;
+    }
 
-        public BootstrapModel(ILogger<BootstrapModel> logger)
-        {
-            _logger = logger;
-        }
+    public void OnGet()
+    {
 
-        public void OnGet()
-        {
+    }
 
-        }
-
-        public void OnPost()
-        {
-            _logger.LogWarning("On Post Called!!!!");
-        }
+    public void OnPost()
+    {
+        _logger.LogWarning("On Post Called!!!!");
     }
 }
